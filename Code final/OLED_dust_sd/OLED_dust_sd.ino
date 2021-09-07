a7ac40a05865c94fccb8e6c036e6556be65a0323
amandamarielewis <amandalewis@newschool.edu>
2021-09-07 23:21:00 +0200

    update

---
+++ Code final/OLED_dust_sd/OLED_dust_sd.ino
@@ -0,0 +1,94 @@
+//Bibliothèques incluses dans le sketch
+
+#include <U8g2lib.h>
+#include <U8x8lib.h>
+
+#include "U8glib.h"
+#include <U8glib.h>
+
+#include <Wire.h>
+#include <GP2YDustSensor.h>
+#include <SPI.h>
+#include <SD.h>
+
+//Initialiser notre type spécifique d'écran OLED
+//U8GLIB_SH1106_128X64 u8g(U8G_I2C_OPT_DEV_0|U8G_I2C_OPT_FAST);  // Dev 0, Fast I2C / TWI
+U8G2_SH1106_128X64_NONAME_1_HW_I2C u8g2(U8G2_R0, /* reset=*/ U8X8_PIN_NONE);
+
+
+const uint8_t SHARP_LED_PIN = 10;   // Sharp poussière/particle capteur Led Pin
+const uint8_t SHARP_VO_PIN = A0;    // Sharp poussière/particle pin de sortie analogique utilisé pour la lecture 
+
+//Initializer le PM2.5 capteur de poussière
+GP2YDustSensor dustSensor(GP2YDustSensorType::GP2Y1010AU0F, SHARP_LED_PIN, SHARP_VO_PIN);
+
+const int chipSelect = 4;
+
+void setup(void) {
+  // mettez votre code d'installation ici, à exécuter une fois
+  Serial.begin(9600);
+  Serial.print("Initializing SD card...");
+  dustSensor.begin();
+
+
+  // voir si la carte est présente et peut être initialisée :
+  if (!SD.begin(chipSelect)) {
+    Serial.println("Card failed, or not present");
+    //don't do anything more:
+    //while (1);
+  }
+  Serial.println("card initialized.");
+
+  //dustSensor.setBaseline(0.4); // set no dust voltage according to your own experiments
+  //dustSensor.setCalibrationFactor(1.1); // calibrate against precision instrument
+
+  u8g2.begin();  
+}
+
+void loop(void) {
+  // mettez votre code principal ici, pour l'exécuter de façon répétée :
+  // Serial Print dust sensor data
+   Serial.print("Dust density: ");
+   Serial.print(dustSensor.getDustDensity());
+  // Serial.print(" ug/m3; Running average: ");
+  // Serial.print(dustSensor.getRunningAverage());
+  // Serial.println(" ug/m3");
+
+  // faire une string pour assembler les données à enregistrer :
+  String dataString = "";
+  
+  File dataFile = SD.open("datalog.txt", FILE_WRITE);
+
+  if (dataFile) {
+    dataFile.println(dustSensor.getDustDensity());
+    dataFile.close();
+    Serial.print("writing to file: ");
+    Serial.print(dustSensor.getDustDensity());
+    Serial.println(" ug/m3");
+  } else {
+    Serial.println("error opening file");
+  }
+˙˙
+//Écran OLED impression poussière capteur données image boucle
+
+  u8g2.firstPage();  
+  do {
+      draw();
+  } while( u8g2.nextPage() );
+
+  //delay before we run again
+  delay(1000);
+}
+
+void draw(void) {
+  // graphic commands to redraw the complete screen should be placed here  
+   u8g2.clearBuffer();          // clear the internal memory
+   u8g2.setFont(u8g2_font_ncenB08_tr); // choose a suitable font˙
+//  u8g2.setFont(u8g_font_unifont);
+  u8g2.setFont(u8g_font_osb21);
+  u8g2.setCursor(0, 20); 
+  u8g2.print("Dust : ");
+  u8g2.print(dustSensor.getDustDensity());
+  u8g2.print(" ug/m3");
+     u8g2.clearBuffer();          // clea
+}
--- README.md
+++ README.md
@@ -1,6 +1,9 @@
 # Atelier Recensement Collectif
 Github pour l'atelier de détection collective du festival DIWO à Stereolux à Nantes le 8 septembre 2021.
 
+[Lien vers la présentation technique.](https://docs.google.com/presentation/d/1s3T18qGx1d3UmDFFPPqAXClwZHRb0sIQyXGvgYmdmSk/edit?usp=sharing)
+
+
 **Matériel**
 
 * Arduino et cordon
@@ -16,12 +19,14 @@ Github pour l'atelier de détection collective du festival DIWO à Stereolux à 
 **Logiciel**
 
 * Arduino IDE [(Téléchargez-le ici)](https://www.arduino.cc/en/software)
+* [Driver pour les ordinateurs Windows avec cet Arduino, au cas où il ne serait pas reconnu.](https://sparks.gogo.co.nz/ch340.html)
 
 Librairies Arduino
 
 * Sharp Dust library [(Documentation ici)](https://github.com/luciansabo/GP2YDustSensor)
 * U8G2
 * U8Glib
+* SD
 
 ## Obtenir des données : Configuration du capteur de poussière
 
@@ -31,7 +36,16 @@ Librairies Arduino
 
 ![Image of Dust sensor and OLED setup](https://github.com/amandamarielewis/scarred-landscapes-workshop/blob/main/images/ASSL_workshop_dustoled_bb.jpg)
 
+* Le brochage de l'écran OLED est différent de celui du schéma. Les broches 5V et GND sont commutées, assurez-vous donc que la broche VCC est connectée à l'alimentation (+) et que la broche GND est connectée à la terre (-).
+
+```
+U8GLIB_SH1106_128X64 u8g(U8G_I2C_OPT_DEV_0|U8G_I2C_OPT_FAST);	// Dev 0, Fast I2C / TWI
+```
+
 ## La portabilité : sauvegarder vos données sur des cartes SD
 
 ![Image of Dust sensor, OLED, SD card setup](https://github.com/amandamarielewis/scarred-landscapes-workshop/blob/main/images/ASSL_workshop_complete_bb.png)
 
+## Questions
+
+Les problèmes courants ou les corrections de bogues seront notés ici.
